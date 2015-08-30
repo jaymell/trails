e549eb4e26adf12ba1c44864466d58e3049a5116
james <jamesmlucas@gmail.com>
2015-08-30 02:05:13 -0500

    Code in place for import_data, needs testing

--- import_data.py
+++ import_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import os
+import shutil
 import uuid
 import zipfile
 import datetime
@@ -8,12 +9,7 @@ import dateutil.tz
 import xml.etree.ElementTree as ET
 import json
 from pymongo import MongoClient
-
-
-""" iterate through kmz files,
-	extract to unique folder name
-	open kml file -- get date/time and location
-"""
+import re
 
 MONGODB_HOST = 'localhost'
 MONGODB_PORT = 27017
@@ -21,84 +17,109 @@ DB_NAME = 'fitness'
 connection = MongoClient(MONGODB_HOST, MONGODB_PORT)
 collection = 'trails'
 
-kmz_dir = './kmz'
-utc = dateutil.tz.tzutc() 
-central = dateutil.tz.gettz('America/Chicago')
-out_file = 'inventory.json'
 
 def Kml(kml):
+	""" for getting data out of google tracks kml files """ 
 
 	def _get_time(self):
 		""" get time in first 'when' tag that appears --
 			there may be a better way to do this """ 
 		# need to variable-ize the version ??? 
-		time = root.find('.//{http://www.opengis.net/kml/2.2}when').text
+		time = self.root.find('.//{http://www.opengis.net/kml/2.2}when').text
 		## strip off last 5 chars, ie '.135Z in '2015-08-01T00:06:29.135Z'
+		utc = dateutil.tz.tzutc() 
+		central = dateutil.tz.gettz('America/Chicago')
 		time = datetime.datetime.strptime(time[:-5], '%Y-%m-%dT%H:%M:%S')
 		time = time.replace(tzinfo=utc)
-		time = time.astimezone(central)
-
-	def _get_activity(self):
-		pass
+		self.time = time.astimezone(central)
 
 	def __init__(self, kml):
+		""" expects to be passed a file handle for kml file """
+
+		self.uid = uuid.uuid4()
 		self.kml = kml
-		self.tree = ET.parse(kml)
+		self.tree = ET.parse(self.kml)
 		self.root = tree.getroot()
-		self.name = root.find('.//{http://www.opengis.net/kml/2.2}name').text
-		self.time = self._get_time()
+
+		self.name = self.root.find('.//{http://www.opengis.net/kml/2.2}name').text
+		self.activity = self.root.find('.//{http://www.opengis.net/kml/2.2}Data').text
+
+		self._get_time()
+		# just a big string of text, but there is lots
+		# to pull out of it, eg activity type:
+		self.description = self.root.find('.//{http://www.opengis.net/kml/2.2}description').text
+
+	def as_json(self):
+		return json.dumps({'name': self.name, 'activity': self.activity, 'date': self.time.strftime('%Y-%m-%d %H:%M:%S'), 'uid': uid.__str__()})
 		
-def is_duplicate(item, collection):
+		
+def is_duplicate(kml, collection):
 	""" check whether a record with EXACT same
 		date already exists in given mongo collection
 	"""
 	
 	results = [ i for i in collection.find({'date': item['date']}) ]
 	if results:
-		print
-		print('Duplicate found! %s' % item)
-		print
+		print('\nDuplicate found! %s\n' % item)
 		return True
 	else:
 		return False
 
-for kmz in os.listdir(kmz_dir):
-	if kmz[-3:] == 'kmz':
-		try:
-			uid = uuid.uuid4()
-			zipped = zipfile.ZipFile(os.path.join(kmz_dir, kmz))
-			kml = zipfile.ZipFile.open('doc.kml')
-		except Exception as e:
-			print('Failed for %s: %s' % (kmz, e))
-		else:
-			print('FILE NAME: %s' % kmz)
-			# open doc.kml in appropriate folder
-			# open xml parser, get date
-
-			print('Name: %s\tTime: %s' % (name, time) )
-			# convert to dict:
-			item = { 'uid': uid.__str__(), 'date': time.strftime('%Y-%m-%d %H:%M:%S'), 'name': name }
-			# append to list if it's not a duplicate:
-			if not is_duplicate(item, collection): 
-				# try to update database AND extract
-				# extract files to right place; if one
-				# fails, undo the other:	
+def process(kml_file, kmz=False):
+	""" expects to be passed either a kmz or kml file """
+	try:
+		if kmz:
+			zipped = zipfile.ZipFile(kml_file)
+			kml = Kml(zipped.open('doc.kml'))
+		else: 
+			kml = Kml(open(kml_file))
+	except Exception as e:
+		print('Failed for %s: %s' % (kml_file, e))
+	else:
+		print('FILE NAME: %s' % kml_file)
+		if not is_duplicate(kml.as_json(), collection): 
+			# try to update database AND
+			# extract files to right place; if one
+			# fails, undo the other:	
+			try:
+				collection.insert_one(kml.as_json())
+			except Exception as e:
+				print('Failed to update database with %s: %s' % (kml, e))
+			else:
 				try:
-					collection.insert_one(item)
+					dest = 'static/kml/%s' % kml.uid
+					if kmz:
+						zipped.extractall(dest)
+					else:
+						if not os.path.exists(os.path.dirname(dest)):
+							os.makedirs(os.path.dirname(dest))
+							shutil.copy(kml_file, '%s/doc.kml' % dest)
 				except Exception as e:
-					print('Failed to update database with %s: %s' % (item, e))
-				else:
+					print('Failed to extract files: %s\n\tTring to remove record from database...' % e)
 					try:
-						zipped.extractall('static/kml/%s' % uid)
+						collection.remove(kml.as_json())
 					except Exception as e:
-						print('Failed to extract files: %s\n\tTring to remove record from database...' % e)
-						try:
-							collection.remove(item)
-						except Exception as e:
-							print('Failed to remove item from database -- db is no longer consistent w/ file system: %s' % e)
-		finally:
+						print('Failed to remove item from database -- db is no longer consistent w/ file system: %s' % e)
+	finally:
+		if kmz:
 			zipped.close()
+		else:
+			kml.close()
 		
+if __name__ == '__main__':
+	""" iterate through files (currently specified by location below) -- 
+		determine whether kmz or kml and call process()
+		appropriately """
+
+	location = './kmz'
+	for item in os.listdir(location):
+		if item[-3:] == 'kmz':
+			process(os.path.join(location,item), kmz=True)
+		elif item[-3:] == 'kml':
+			process(os.path.join(location,item))
+
+		
+
 """
 # first when tag:
 root.find('.//{http://www.opengis.net/kml/2.2}Placemark//{http://www.opengis.net/kml/2.2}when').text
